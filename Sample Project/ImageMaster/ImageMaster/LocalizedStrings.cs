c64022ac2ea47ce6e1a9064506f5eaf79420d548
dhelper <dror.helper@gmail.com>
2014-04-01 21:09:12 +0300

    Added all samples and project

---
+++ Sample Project/ImageMaster/ImageMaster/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using ImageMaster.Resources;
+
+namespace ImageMaster
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
